c2f02335e3f4c35b6f061631990c41557292be7c
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-03-16 09:20:59 +0530

    store created

--- CRUD/src/main.jsx
+++ CRUD/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react";
+import { configureStore } from "@reduxjs/toolkit";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const store = configureStore({
+  reducer: {
+    // user :
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
